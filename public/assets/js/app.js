7a221fbd22e4308b9448fd84f6d55c1d5f3a722e
justinpolchinski <justinpolchinski@gmail.com>
2018-06-08 21:59:52 -0400

    Everything is working hmmm i think

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,154 @@
 console.log("Linking...app.js");
-
+$( document ).ready(function() {
+   let thisId;
+   let parentLi;
+   let thisNote;
+   let noteId;
 $("#scraped").on("click", function() {
     $.ajax({
         method: "GET",
         url: "/scrape",
     }).done(function(data) {
         console.log(data)
-        window.location = "/"
+        window.location.reload(true);
     })
+});
+
+$(".saveArticle").on("click", function(event) {
+    thisId = $(this).attr("data-id");
+    parentLi = $(event.target).parent("li");
+    
+    $.ajax({
+        method: "POST",
+        url: "/saved/" + thisId,
+        
+    }).then(function(data) {
+        console.log("saved");
+        console.log(data);
+        parentLi.empty();
+    })
+});
+
+$(".deleteArticle").on("click", function(event) {
+    thisId = $(this).attr("data-id");
+    parentLi = $(event.target).parent("li");
+    $.ajax({
+        method: "POST",
+        url: "/unsaved/" + thisId,
+        
+    }).then(function(data) {
+        console.log("removed");
+        console.log(data);
+        parentLi.empty();
+        
+    });
+});
+
+$("#saved").on("click", function() {
+    
+    $.ajax({
+        method: "GET",
+        url: "/savedArticles"
+        
+    }).done(function(data) {
+        console.log("saved Articles button");
+       window.location="/savedArticles";
+    })
+});
+
+
+
+$(".openNotes").on('click',(event)=>{
+    
+    thisNote =$(event.target).attr("data-id");
+    
+    sessionStorage.thisNote = thisNote;
+    
+    console.log("thisNote data-id: " + thisNote);
+    $.ajax({
+        method: "GET",
+        url: "/savedNotes/"+thisNote
+    })
+    .then((data)=>{
+        console.log(JSON.stringify(data, null, 4));
+        console.log(data.note);
+        
+         if (!data.note){
+             console.log("else no data.note");
+           $("#notesTaken").html("");
+           $("#modalTitle").html("");
+           $("#inpT").attr("placehoder","Enter Note title");
+           $("#inpB").attr("placehoder","Enter Note");
+            //return;
+        }
+        if(data.note){
+        noteId = data.note._id;
+        sessionStorage.noteId = noteId;
+        console.log("OpenNotes: " + data);
+        $("#inpT").empty();
+        $("#inpB").empty();
+        $("#modalTitle").text(data.note.title);
+        $("#notesTaken").text(data.note.body);
+        }
+////////////////////////////////
+$(".deleteNote").on("click",()=>{
+    console.log("noteId: " + noteId);
+    noteId = sessionStorage.noteId;
+    thisNote = sessionStorage.thisNote;
+    $.ajax({
+        method: "DELETE",
+        url: "/delete/" + noteId + "/" + thisNote,
+        
+    }).then((data)=>{
+        console.log("deleting notes " +data);
+        $(".modal").modal("hide");
+        $("#notesTaken").html("");
+        $("#modalTitle").html("");
+    })
+    
+
+})       
+///////////////////////////////
+
+        $(".takeNote").on("click", ()=>{
+           
+         
+           thisNote = sessionStorage.thisNote;
+            $.ajax({
+                method: "POST",
+                url: "/notes/" + thisNote,
+                data:{
+                    title: $(".titleInput").val(),
+                    body: $(".bodyInput").val()
+                }
+            })
+            .then((data)=>{
+                console.log("takeNote: /notes/" + thisNote + ' '+ data.note.title);
+            })
+                $(".modal").modal("hide");
+               
+            
+        })
+        
+        
+    })
+})
+// $(".deleteNote").on("click",()=>{
+//     console.log("noteId: " + noteId);
+//     noteId = sessionStorage.noteId;
+//     thisNote = sessionStorage.thisNote;
+//     $.ajax({
+//         method: "POST",
+//         url: "/delete/" + noteId + "/" + thisNote,
+        
+//     }).then((data)=>{
+//         console.log("deleting notes " +data);
+//         $(".modal").modal("hide");
+//         $("#notesTaken").html("");
+//         $("#modalTitle").html("");
+//     })
+    
+
+// })
+//end of doc ready
 });
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//import { fail } from "assert";
+
 
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -30,15 +32,26 @@ mongoose.connect("mongodb://localhost/adafruitdb");
 //Scraping Adafruit
 
 app.get('/', function (req, res) {
-    
+
    db.Article.find({"saved": false}, function(error, data) {
         var hbsObject = {
           article: data
         };
-        console.log(hbsObject);
+        console.log("handleBars OBJECT: " + hbsObject);
         res.render("home", hbsObject);
       });
     });
+    //saved article page
+    app.get('/savedArticles', function (req, res) {
+      
+      db.Article.find({"saved": true}, function(error, data) {
+           var hbsObject = {
+             article: data
+           };
+           console.log(hbsObject);
+           res.render("savedArticles", hbsObject);
+         });
+       });
 //});
 app.get("/scrape", function(req,res){
     
@@ -46,33 +59,127 @@ app.get("/scrape", function(req,res){
        
     var $ = cheerio.load(html);
     
-    //var titleCheck;
+    var titleCheck;
     $(".masonry-block").each(function(i, element) {
+    
+    
     var results = {};
     results.title = $(this).find('.title').find("a").html().split(/\n/g)[1].split(/<br>/g)[0];
     results.url = "https://learn.adafruit.com" + $(this).find('.title').find("h5").html().split(/href="/g)[1].split(/">/g)[0];
     results.author = $(this).find(".author").find("a").html().split(/name">/g)[1].split(/<\/span>/g)[0];
     results.description = $(this).find(".description").html().split(/">\n/g)[1].split(/\n</g)[0];
     results.imgVideo = $(this).find("video, img");
-    console.log("results "+[i]+" title: " + results.title);
-    console.log("results "+[i]+" url: " + results.url);
-    console.log("results "+[i]+" author: " + results.author);
-    console.log("results "+[i]+" description: " + results.description);
-    console.log("results "+[i]+" imgVideo: " + results.imgVideo);
-    db.Article.create(results).then(function(dbArticle){
-        console.log("create: " + dbArticle);
-        res.send("Scrape Completed!!!");
-    }).catch(function(err){
-        console.log("scrape catch");
-       return res.json(err);
+    // console.log("results "+[i]+" title: " + results.title);
+    // console.log("results "+[i]+" url: " + results.url);
+    // console.log("results "+[i]+" author: " + results.author);
+    // console.log("results "+[i]+" description: " + results.description);
+    // console.log("results "+[i]+" imgVideo: " + results.imgVideo);
+    db.Article.findOne({title: results.title},(error,found)=>{
+      if(error){
+        console.log("titleCheck: " + error);
+      }
+      else if (found){
+         
+        return;
+        
+      }
+      else{
+        db.Article.create(results).then(function(dbArticle){
+            console.log("create: " + dbArticle);
+            res.send("Scrape Completed!!!");
+        }).catch(function(err){
+            console.log("scrape catch");
+          return res.json(err);
+        });
+      } 
+        });
+      });
     });
-   
-});
+    });
+
+// save article
+app.post("/saved/:id", function(req,res){
+  db.Article.findOneAndUpdate({_id: req.params.id}, {"saved":true})
+  .then((data)=>{
+    res.json(data);
+    })
+    .catch((err)=>{
+  res.json(err);
+    })
   
-  });
+});
+//unsave article
+app.post("/unsaved/:id", function(req,res){
+  db.Article.findOneAndUpdate({_id: req.params.id}, {"saved":false})
+    .then((data)=>{
+        res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    })
   
 });
 
+  
+  
+  app.post("/notes/:id", function(req, res) {
+    console.log("req.body: " + req.body.title +" "+ req.body.body);
+  // Create a new note and pass the req.body to the entry
+    
+    db.Note.create(req.body)
+    .then(function(dbNote) {
+      console.log("dbNote: " + dbNote);
+      console.log("params id: " + req.params.id);
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    }).then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+  
+  
+  });
+
+  app.get("/savedNotes/:id",(req,res)=>{
+    console.log("/savedNotes/ " + req.params.id);
+    db.Article.findOne({_id: req.params.id})
+    .populate("note")
+    .then((dbArticle)=>{
+      res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+  })
+
+});
+
+app.delete("/delete/:note_id/:article_id", function(req, res) {
+  // Use the note id to find and delete it
+  db.Note.findOneAndRemove({ "_id": req.params.note_id }, function(err) {
+    // Log any errors
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+    else {
+      db.Article.findOneAndUpdate({ "_id": req.params.article_id }, {$pull: {note: req.params.note_id}})
+       // Execute the above query
+        .exec(function(err) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+            res.send(err);
+          }
+          else {
+            // Or send the note to the browser
+            res.send("Note Deleted");
+          }
+        });
+    }
+  });
+});
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
